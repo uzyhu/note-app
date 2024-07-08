86d198439d3b3545e39c79c4450bc3eb986ac528
uzyhu <dnwlgn0807@naver.com>
2024-07-09 03:07:29 +0900

    add: date format

--- app.js
+++ app.js
@@ -1,47 +1,65 @@
 const express = require("express");
 const connection = require("./mariadb");
-const cors = require('cors');
+const cors = require("cors");
+const { format } = require("date-fns");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/notes', (req, res) => {
-    const query = 'SELECT * FROM notes';
-    connection.query(query, (err, results) => {
-        if (err) {
-            res.status(500).send('Error fetching users');
-            return;
-        }
-        console.log("results", results);
-        res.json(results);
-    });
-});
+app.get("/notes", (req, res) => {
+  const query = "SELECT * FROM notes";
+  connection.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send("Error fetching users");
+      return;
+    }
 
-app.post('/notes', (req, res) => {
-  const { title, content } = req.body;
-  const query = 'INSERT INTO notes (title, content) VALUES (?, ?)';
-  connection.query(query, [title, content], (err, results) => {
-      if (err) {
-          res.status(500).send('Error adding note');
-          return;
-      }
-      res.status(201).json({ id: results.insertId, title, content });
+    const formattedResults = results.map((note) => {
+      const formattedCreatedAt = format(
+        new Date(note.createdAt),
+        "yyyy-MM-dd HH:mm:ss"
+      );
+      const formattedUpdatedAt = format(
+        new Date(note.updatedAt),
+        "yyyy-MM-dd HH:mm:ss"
+      );
+      return {
+        ...note,
+        createdAt: formattedCreatedAt,
+        updatedAt: formattedUpdatedAt,
+      };
+    });
+
+    console.log("results", formattedResults);
+    res.json(formattedResults);
   });
 });
 
-app.delete('/notes/:id', (req, res) => {
+app.post("/notes", (req, res) => {
+  const { title, content } = req.body;
+  const query = "INSERT INTO notes (title, content) VALUES (?, ?)";
+  connection.query(query, [title, content], (err, results) => {
+    if (err) {
+      res.status(500).send("Error adding note");
+      return;
+    }
+    res.status(201).json({ id: results.insertId, title, content });
+  });
+});
+
+app.delete("/notes/:id", (req, res) => {
   const { id } = req.params;
-  const query = 'DELETE FROM notes WHERE id = ?';
+  const query = "DELETE FROM notes WHERE id = ?";
   connection.query(query, [id], (err, results) => {
-      if (err) {
-          res.status(500).send('Error deleting note');
-          return;
-      }
-      res.status(204).send();
+    if (err) {
+      res.status(500).send("Error deleting note");
+      return;
+    }
+    res.status(204).send();
   });
 });
 
 app.listen(8080, () => {
-    console.log(`Server running on port 8080`);
+  console.log(`Server running on port 8080`);
 });
